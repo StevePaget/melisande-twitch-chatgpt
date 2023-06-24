b094d09393d1528593d534b2d17abc25657e2517
Steve Paget <StevePaget@users.noreply.github.com>
2023-06-24 14:40:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ app.get('/gpt/:text', async (req, res) => {
           console.log ("Sliced agent answer: " + agent_response)
         }
 
-        res.send(agent_response)
+        res.send("<meta charset='UTF-8'>" + agent_response)
       } else {
         res.send("Something went wrong. Try again later!")
       }
